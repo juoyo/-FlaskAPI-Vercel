a51833bf8932b777efe23cc270c11ddd2695b34b
juoyo <juoyohub@gmail.com>
2021-06-04 23:45:40 +0800

    2345

---
+++ api/calculator.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response, request, redirect, url_for
+import sympy
+from sympy.abc import x, y, z
+
+
+app = Flask(__name__)
+
+@app.route('/api/calculator',methods = ['POST', 'GET'])
+def login():
+  if request.method == 'POST':
+      cal_para1 = request.form['cal_para1']
+  else:
+      cal_para1 = request.args.get('cal_para1')
+
+  result = "sympy"
+  return  result
+
+
+
+
